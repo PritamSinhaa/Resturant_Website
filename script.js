1fa1364a7797b1477ce9a306b9a12b89c13fd425
Pritam Sinha <pritamsinha2323548@gmail.com>
2025-06-21 12:40:36 +0530

    changed

--- script.js
+++
@@ -1,9 +0,0 @@
-    function addNumbers() {
-      let a = parseFloat(document.getElementById("num1").value);
-      let b = parseFloat(document.getElementById("num2").value);
-      let sum = a + b;
-      document.getElementById("result").textContent = "Sum: " + sum;
-    }
-    
-  let num = parseFloat(prompt("Enter a number:"));
-  console.log("You entered:", num);
